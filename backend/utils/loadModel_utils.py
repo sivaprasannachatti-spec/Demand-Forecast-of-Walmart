dd35531b558f2348b562c07b1ba02740c5dbd6e4
Shiva <shiva@example.com>
2026-02-15 11:49:54 +0530

    model updated successfully

--- backend/utils/loadModel_utils.py
+++ backend/utils/loadModel_utils.py
@@ -1,13 +1,15 @@
 import sys
-import dill
+import os
+import joblib
+import numpy as np
 
 from src.exception import CustomException
+from src.logger import logging
 
 def loadModel():
     try:
-        MODEL_PATH = 'artifacts/model.pkl'
-        with open(MODEL_PATH, 'rb') as f:
-            model = dill.load(f)
+        MODEL_PATH = 'model.pkl'
+        model = joblib.load(MODEL_PATH)
         return model
     except Exception as e:
         raise CustomException(e, sys)
